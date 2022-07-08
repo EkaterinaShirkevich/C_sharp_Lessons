82d5267b992d280a7326a282763ea4be208f8efc
Ekaterina <eshirkevich@gmail.com>
2022-07-08 11:09:38 +0300

    add 1 task

---
+++ 1_lesson/1_lesson_1/1/Program.cs
@@ -0,0 +1,5 @@
+﻿//программа, выводящая квадрат числа, вводимого пользователем
+Console.WriteLine("Write a number: ");
+string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+Console.WriteLine(a*a);
