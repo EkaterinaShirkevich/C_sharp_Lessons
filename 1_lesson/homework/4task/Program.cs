65593f1e1e83c88a5269a2577c7daef2eb32856c
Ekaterina <eshirkevich@gmail.com>
2022-07-09 15:08:06 +0300

    add hw 4task

---
+++ 1_lesson/homework/4task/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine());
+int i=2;
+if(i>a)
+{
+    Console.WriteLine(a-1);
+}
+while(i<=a)
+{
+    Console.WriteLine(i);
+    i=i+2;
+}
