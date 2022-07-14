48a0ab9987832dffe0c42b5f380adb25a35a7eed
Ekaterina <eshirkevich@gmail.com>
2022-07-14 16:39:10 +0300

    add 4task HW

---
+++ 2_lessson/homework/4task/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на
+//вход цифру, обозначающую день недели, и проверяет,
+//является ли этот день выходным.
+
+Console.Write("Enter the Number: ");
+int Number=int.Parse(Console.ReadLine());
+if (Number>5)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+if(Number>7)
+{
+    Console.WriteLine("Error");
+}
+
+
+
