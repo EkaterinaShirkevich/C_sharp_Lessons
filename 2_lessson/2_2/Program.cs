c9cd18eaf8be9a2ab3b601bfea092321438a58cd
Ekaterina <eshirkevich@gmail.com>
2022-07-14 14:44:55 +0300

    add 2_2

---
+++ 2_lessson/2_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру числа
+
+int MaxDigit(int Number) //MaxDidit - название, которое мы даем методу(функции), Number - название, кот. мы даем переменной
+{
+    if(Number/10>Number%10)
+       return Number/10;
+    else
+       return Number%10;
+}
+int a=new Random().Next(10,100); 
+Console.WriteLine(a);
+Console.WriteLine(MaxDigit(a)); //вызываем метод
