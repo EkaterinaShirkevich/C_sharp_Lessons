a7a4ac8681497480cac6abe1f71d63695a258435
Ekaterina <eshirkevich@gmail.com>
2022-07-17 14:26:54 +0300

    add hw 3task

---
+++ 2_lessson/homework/3task/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+void ThirdNumber(int num)
+{
+    if(num<100)
+    {
+        Console.WriteLine("Enter correct number");
+        return;
+    }
+    while(num>999)
+    num=num/10;
+    Console.WriteLine(num%10);
+}
+Console.WriteLine("Enter the number: ");
+int num =int.Parse(Console.ReadLine());
+ThirdNumber(num);
