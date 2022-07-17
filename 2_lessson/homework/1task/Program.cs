f66af8f8c14afe5282c13ef9fbc910459fa824fe
Ekaterina <eshirkevich@gmail.com>
2022-07-17 13:50:44 +0300

    add hw 1task

---
+++ 2_lessson/homework/1task/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int Number(int Num)
+{
+    return Num/10%10;
+}
+Console.WriteLine("Enter three-digit number: ");
+int num=int.Parse(Console.ReadLine());
+if(num>99 && num<1000)
+{
+   Console.WriteLine(Number(num)); 
+}
+else
+{
+   Console.WriteLine("Error, please, try again");
+}
+
+    
