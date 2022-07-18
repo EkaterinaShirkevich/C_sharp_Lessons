66afd1ce478905a34b6067f712b24c9f5361c69c
Ekaterina <eshirkevich@gmail.com>
2022-07-18 15:27:39 +0300

    add 3_3

---
+++ 3_lesson/3_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// программа принимает на вход число N и выдает таблицу квадратов чисел от 1 до N
+
+void Searching(int a)
+{
+    int i=1;
+    while(i<=a)
+    {
+        Console.WriteLine(Math.Pow(i,2));
+        i++;
+    }
+}
+Searching(7);
