3e8d60aca5421185b2bbf09ad549eccf23461603
Ekaterina <eshirkevich@gmail.com>
2022-07-20 23:06:17 +0300

    add hw 1task

---
+++ 3_lesson/homework/1task/Program.cs
@@ -0,0 +1,19 @@
+﻿// : Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно
+//палиндромом.
+//14212 -> нет
+//23432 -> да
+//12821 -> да
+
+void Palindrome(int a)
+{
+    if(a/1000==(a%10)*10+(a%100)/10)
+    {
+        Console.WriteLine("This is palindrome");
+    }
+    
+    else
+    {
+        Console.WriteLine("This is not palindrome");
+    }
+}
+Palindrome(23432);
