214d1130124a1289a7c49c8118d17552afa10e9d
Ekaterina <eshirkevich@gmail.com>
2022-08-03 13:43:35 +0300

    add 4_3

---
+++ 4_lesson/4_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// программа выводит массив из 8 эл-тов, заполненный 0 и 1 в случайном порядке
+
+void Random01Array(int num)
+{
+    int[] array= new int[num];
+    for(int i=0; i<num; i++)
+    {
+        array[i]=new Random().Next(0,2); // 2 не захватывает, идет от 0 до 1
+        Console.WriteLine($"{array[i]} ");
+    }
+}
+Random01Array(8);
