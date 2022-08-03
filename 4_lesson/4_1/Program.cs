5918a0d0e758fa20150c7669d361613b9b32ee0f
Ekaterina <eshirkevich@gmail.com>
2022-08-03 13:14:52 +0300

    add 4_1

---
+++ 4_lesson/4_1/Program.cs
@@ -0,0 +1,8 @@
+﻿// программа принимает на вход число и выдает кол-во цифр в числе
+
+void Lenght(int a)
+{
+    string num = a.ToString(); //преобразуем число в строку
+    Console.WriteLine(num.Length);
+}
+Lenght(1670);
