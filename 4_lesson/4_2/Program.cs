55e95236bc901fcc18b2e853abf037ce6bd857e3
Ekaterina <eshirkevich@gmail.com>
2022-08-03 13:30:12 +0300

    add 4_2

---
+++ 4_lesson/4_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// программа принимает число N и выдвет произведение чисел от 1 до N
+
+int Method(int num)
+{
+    int allsum=1;
+    for( int i=1; i<=num; i++)
+    allsum=allsum*i;
+    return allsum;
+}
+Console.WriteLine(Method(5));
