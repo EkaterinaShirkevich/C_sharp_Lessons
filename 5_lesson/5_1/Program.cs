111754fa4aba43ebe6c5d696574390f43417a8df
Ekaterina <eshirkevich@gmail.com>
2022-08-05 11:50:56 +0300

    5_1 changes

--- 5_lesson/5_1/Program.cs
+++ 5_lesson/5_1/Program.cs
@@ -16,22 +16,23 @@ int[] MassNums(int size, int from, int to)
     int[] arr = new int[size];
 
     for (int i = 0; i < size; i++)
-int[]arr_1=
     {
         arr[i] = new Random().Next(from, to);        
     }    
     return arr;
 }
 
-void Change(int [] arr)
+void Ism(int[] arr)
 {
-    for(int i=0; i<arr.Length; i++)
-    {
-        arr[i]=arr[i]*-1;
+
+    for(int i = 0; i < arr.Length; i++)
+
+        arr[i] = arr[i] * -1;
        
-    }
 }
-MassNums(12,-9,9);
+
+int[] arr_1 = MassNums(12, -9, 10);
 Print(arr_1);
-Change(arr_1);
-Print(arr_1);    
+Ism(arr_1);
+Print(arr_1);
+
