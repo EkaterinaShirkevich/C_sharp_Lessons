a2fb42a8dd493441c72e5b995a824cc77fb41663
Ekaterina <eshirkevich@gmail.com>
2022-08-05 11:44:32 +0300

    5_4

---
+++ 5_lesson/5_4/Program.cs
@@ -0,0 +1,45 @@
+﻿//найти произведение пар чисел в массиве, парой считается первый и последний,
+// второй и предпоследний
+//результат записать в новом массиве
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(1, 10);
+    }
+    return arr;
+}
+
+int[] PairsNum(int[] arr)
+{
+    int size = arr.Length;
+    int flex_size = size / 2 + size % 2;
+    int[] new_arr = new int[flex_size];
+
+    for(int i = 0; i < size / 2; i++)
+        new_arr[i] = arr[i] * arr[size - i - 1];
+
+    if (new_arr[flex_size - 1] == 0)
+        new_arr[flex_size - 1] = arr[flex_size - 1];
+
+    return new_arr;
+}
+
+int[] arr_1 = MassNums(4);
+Print(arr_1);
+int[] new_arr_1 = PairsNum(arr_1);
+Print(new_arr_1);
