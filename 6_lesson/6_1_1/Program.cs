db91eba3222a0dfd618ab70fec64f5a6a445046b
Ekaterina <eshirkevich@gmail.com>
2022-08-09 10:46:10 +0300

    6_1

---
+++ 6_lesson/6_1_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// неравенство треугольника - программа принимает числа и проверяет
+//есть ли треугольник со сторонами такой длины
+
+void Triangle(int x, int y, int z)
+{
+    if(x+y>z && x+z>y && y+z>x)
+        Console.WriteLine("True");
+    else
+        Console.WriteLine("False");    
+}
+Triangle(4,2,5);
