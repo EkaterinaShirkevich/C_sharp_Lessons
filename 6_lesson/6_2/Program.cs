532c3c1d4d0714cb28f60138acd09ffdf02bf33c
Ekaterina <eshirkevich@gmail.com>
2022-08-09 10:56:49 +0300

    6_2

---
+++ 6_lesson/6_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// программа преобразовывает десятичное число в двоичное
+
+string ChangeNum(int num)
+{
+    string result = "";
+    while (num > 0)
+    {
+        result = num % 2 + result;
+        num /= 2;
+    }
+    return result;
+}
+Console.WriteLine(ChangeNum(12));
