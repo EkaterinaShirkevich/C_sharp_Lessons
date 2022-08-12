0c8b080771c9d20d538ca3431695310133a3979b
Ekaterina <eshirkevich@gmail.com>
2022-08-12 16:31:03 +0300

    7_2_correct

--- 7_lesson/7_2/Program.cs
+++ 7_lesson/7_2/Program.cs
@@ -1,36 +1,50 @@
 ﻿// // 2. Задайте двумерный массив. Найдите элементы,
 //    у которых обе позиции чётные, и замените эти элементы на их квадраты.
 
-void Print2xArray(int[,] arr);
-int[,] Fill2xArray(int row, int column, int min, int max)
+void Print(int[,] arr)
 {
-    int[,] arr = new int[row,column];
-    
-    for(int i=0; i<row; i++)
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
     {
-        for(int j=0; j<column; j++)
+        for (int j = 0; j < column_size; j++)
         {
-            arr[i,j]=new Random().Next(min,max+1);
+            Console.Write($" {arr[i, j]} ");
         }
         Console.WriteLine();
     }
     Console.WriteLine();
-    return(arr);
 }
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(from, to);
+        }
+    }
+    return arr;
+}
+
 int[,] ReplaceElOn2ndPos2xArray(int[,] arr)
 {
-    for(int i=1; i<arr.GetLength(0); i+=2)
+    for (int i = 1; i < arr.GetLength(0); i += 2)   //arr.GetLength(0) это row
     {
-        for(int j=1; j<arr.GetLength(1); j+=2)
+        for (int j = 1; j < arr.GetLength(1); j += 2) //arr.GetLength(1) это column
         {
-            arr[i,j]*=arr[i,j];
+            arr[i, j] *= arr[i, j];
         }
         Console.WriteLine();
     }
     Console.WriteLine();
-    return(arr);
+    return (arr);
 }
-int[,] arr1 = Fill2xArray(4,4,-10,10);
-Print2xArray(arr1);
+int[,] arr1 = MassNums(4, 4, -10, 10);
+Print(arr1);
 ReplaceElOn2ndPos2xArray(arr1);
-Print2xArray(arr1);
+Print(arr1);
