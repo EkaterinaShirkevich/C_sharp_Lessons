7c1e259f80c73e291f69ca8b1c73e2f7e67060c4
Ekaterina <eshirkevich@gmail.com>
2022-08-12 16:18:51 +0300

    7_1_correct

--- 7_lesson/7_1/Program.cs
+++ 7_lesson/7_1/Program.cs
@@ -7,35 +7,34 @@ void Print(int[,] arr)
     int row_size = arr.GetLength(0);
     int column_size = arr.GetLength(1);
 
-    for(int i=0; i<row_size; i++)
+    for (int i = 0; i < row_size; i++)
     {
-        for(int j=0; j<column_size; j++)
+        for (int j = 0; j < column_size; j++)
         {
-            Console.Write($"{arr[i,j]}");
+            Console.Write($" {arr[i, j]} ");
         }
         Console.WriteLine();
     }
     Console.WriteLine();
 }
 
-int [,] MassNums (int row, int column)
+int[,] MassNums(int row, int column)
 {
-    int [,] arr = new int[row, column];
+    int[,] arr = new int[row, column];
 
-    for (int i=0; i< row; i++)
+    for (int i = 0; i < row; i++)
     {
-        for (int j=0; j< column; j++)
+        for (int j = 0; j < column; j++)
         {
-            arr[i, j] = i+j;
+            arr[i, j] = i + j;
         }
     }
     return arr;
 }
 
-
 Console.Write("Enter the number of rows: ");
 int row = int.Parse(Console.ReadLine());
 Console.Write("Enter the number of columns: ");
 int column = int.Parse(Console.ReadLine());
-int[,] arr_1 = MassNums(row, column, 1, 11);
+int[,] arr_1 = MassNums(row, column);
 Print(arr_1);
