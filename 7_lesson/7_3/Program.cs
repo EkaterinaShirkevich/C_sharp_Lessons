1b0f63a0ede5eaa8a2c8e04f94eaf726c84740b4
Ekaterina <eshirkevich@gmail.com>
2022-08-12 16:47:06 +0300

    7_3_correct

--- 7_lesson/7_3/Program.cs
+++ 7_lesson/7_3/Program.cs
@@ -5,11 +5,11 @@ void Print(int[,] arr)
     int row_size = arr.GetLength(0);
     int column_size = arr.GetLength(1);
 
-    for(int i=0; i<row_size; i++)
+    for (int i = 0; i < row_size; i++)
     {
-        for(int j=0; j<column_size; j++)
+        for (int j = 0; j < column_size; j++)
         {
-            Console.Write($"{arr[i,j]}");
+            Console.Write($" {arr[i, j]} ");
         }
         Console.WriteLine();
     }
@@ -20,33 +20,33 @@ int[,] MassNums(int row, int column, int from, int to)
 {
     int[,] arr = new int[row, column];
 
-    for(int i = 0; i < row; i++)
+    for (int i = 0; i < row; i++)
     {
-        for(int j = 0; j < column; j++)
+        for (int j = 0; j < column; j++)
         {
             arr[i, j] = new Random().Next(from, to);
-        }        
+        }
     }
-    return arr;    
+    return arr;
 }
 
-int SumOfDiagonaile (int [,] arr)
+int SumOfDiagonaleNums(int[,] arr)
 {
-    int count=0;
-    int row=arr.GetLength(0);
-    int column=arr.GetLength(1);
-    if(row<column)
-    column=row;
-    else if(column<row)
-    row=column;
+    int sum = 0;
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
 
-    for(int i=0; i<row; i++)
+    if (row < column)       //урезаем кол-во колонок до кол-ва строк
+        column = row;       //тк матрица дб квадратная
+    else if (column < row)
+        row = column;
+
+    for (int i = 0; i < row; i++)
     {
-        count=count+arr[i,i];
+        sum = sum + arr[i, i];
     }
-    return count;
+    return sum;
 }
-
 
 Console.Write("Enter the number of rows: ");
 int row = int.Parse(Console.ReadLine());
@@ -56,6 +56,6 @@ int column = int.Parse(Console.ReadLine());
 int[,] arr_1 = MassNums(row, column, 1, 11);
 Print(arr_1);
 
-Console.WriteLine(SumOfDiagonaile(arr_1));
+Console.WriteLine(SumOfDiagonaleNums(arr_1));
 
 
