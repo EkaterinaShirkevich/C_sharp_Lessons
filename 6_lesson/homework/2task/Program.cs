e2b957788bec5b7dff0a00d1394c291083e0ea30
Ekaterina <eshirkevich@gmail.com>
2022-08-12 15:43:05 +0300

    somechanges

--- 6_lesson/homework/2task/Program.cs
+++ 6_lesson/homework/2task/Program.cs
@@ -2,22 +2,13 @@
 //пересечения двух прямых, заданных уравнениями 
 //y = k1 *x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
-Console.WriteLine("Enter b1");
-double b1 = double.Parse(Console.ReadLine());
-Console.WriteLine("Enter k1");
-double k1 = double.Parse(Console.ReadLine());
-Console.WriteLine("Enter b2");
-double b2 = double.Parse(Console.ReadLine());
-Console.WriteLine("Enter k2");
-double k2 = double.Parse(Console.ReadLine());
 
 void IntersectionPoint(double b1, double k1, double b2, double k2)
 {
-    double x = (k1 - k2) / (b1 - b2);
+    double x = (b2 - b1)/(k1-k2);
     double y = k2 * x + b2;
     Console.WriteLine($"{x},{y}");
 }
-Console.WriteLine(IntersectionPoint());
-
+IntersectionPoint(2,5,4,9);
 
 
