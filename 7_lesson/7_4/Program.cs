57e5602977ed0e6dc7f4945d57c441439936e0f2
Ekaterina <eshirkevich@gmail.com>
2022-08-12 17:04:27 +0300

    7_4_correct

--- 7_lesson/7_4/Program.cs
+++ 7_lesson/7_4/Program.cs
@@ -7,11 +7,11 @@ void Print(int[,] arr)
     int row_size = arr.GetLength(0);
     int column_size = arr.GetLength(1);
 
-    for(int i=0; i<row_size; i++)
+    for (int i = 0; i < row_size; i++)
     {
-        for(int j=0; j<column_size; j++)
+        for (int j = 0; j < column_size; j++)
         {
-            Console.Write($"{arr[i,j]}");
+            Console.Write($" {arr[i, j]} ");
         }
         Console.WriteLine();
     }
@@ -22,24 +22,24 @@ int[,] MassNums(int row, int column, int from, int to)
 {
     int[,] arr = new int[row, column];
 
-    for(int i = 0; i < row; i++)
+    for (int i = 0; i < row; i++)
     {
-        for(int j = 0; j < column; j++)
+        for (int j = 0; j < column; j++)
         {
             arr[i, j] = new Random().Next(from, to);
-        }        
+        }
     }
-    return arr;    
+    return arr;
 }
 
-string FirstExposition (int [,] arr, int a)
+string FirstExposition(int[,] arr, int a)   //a - искомое число
 {
-    for(int i=0; i<arr.GetLength(0); i++)
+    for (int i = 0; i < arr.GetLength(0); i++)
     {
-        for(int j=0; j<arr.GetLength(1); j++)
+        for (int j = 0; j < arr.GetLength(1); j++)
         {
-            if(arr[i,j]==a)
-                return $"{i+1} {j+1}";
+            if (arr[i, j] == a)
+                return $"{i + 1} {j + 1}";
         }
     }
     return "No";
@@ -50,11 +50,11 @@ Console.Write("Enter the number of rows: ");
 int row = int.Parse(Console.ReadLine());
 Console.Write("Enter the number of columns: ");
 int column = int.Parse(Console.ReadLine());
-
 int[,] arr_1 = MassNums(row, column, 1, 11);
 
 Console.WriteLine("Enter the number");
-int a=int.Parse(Console.ReadLine());
+int a = int.Parse(Console.ReadLine());
+
 Print(arr_1);
 Console.WriteLine(FirstExposition(arr_1, a));
 
