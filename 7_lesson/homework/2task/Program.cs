09a8137d78385b46e7d6c71ae3b9da037e6c5778
Ekaterina <eshirkevich@gmail.com>
2022-08-15 14:26:12 +0300

    HW_2task

--- 7_lesson/homework/2task/Program.cs
+++ 7_lesson/homework/2task/Program.cs
@@ -16,7 +16,7 @@ void Print(int[,] arr)
     {
         for (int j = 0; j < column_size; j++)
         {
-            Console.Write($" {arr[i, j]} ");
+            Console.Write($" {arr[i, j], 6} ");
         }
         Console.WriteLine();
     }
@@ -37,12 +37,25 @@ int[,] MassNums(int row, int column, int from, int to)
     return arr;
 }
 
-string FindWithPosition(int [,] arr, int pos)
-{
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        for (int j = 0; j < arr.GetLength(1); j++)
-        {
-            
-        }
+string FindWithPosition(int [,] arr, int pos1, int pos2)
+{  
+    if(pos1>arr.GetLength(0) | pos2>arr.GetLength(1))
+        return $"This position not found";
+    else
+        return $"{arr[pos1-1,pos2-1]}";  //отнимаем 1 от введенного значения, тк пользователь считает с единицы, а не с нуля
 }
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+int[,] arr_1 = MassNums(row, column, 1, 11);
+
+Print(arr_1);
+
+Console.WriteLine("Enter first element of position");
+int pos1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second element of position");
+int pos2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(FindWithPosition(arr_1, pos1, pos2));
