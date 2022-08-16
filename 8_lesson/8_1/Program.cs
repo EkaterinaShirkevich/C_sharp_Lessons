46bf4452214a2ebee6d39ac512a19fad6d9e3b2b
Ekaterina <eshirkevich@gmail.com>
2022-08-16 10:38:57 +0300

    8_1

---
+++ 8_lesson/8_1/Program.cs
@@ -0,0 +1,60 @@
+﻿// 1. Задайте двумерный массив. Напишите программу,
+//    которая поменяет местами первую и последнюю строку массива.
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+        {
+            Console.Write($" {arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(from, to);
+        }
+    }
+    return arr;
+}
+
+int [,] ReverseFirstEndRow( int [,]arr)
+{
+    int vrem = 0;
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        vrem  = arr[0,j]; //переливание - через временную переменную
+        arr[0,j] = arr[arr.GetLength(0)-1,j];
+        arr[arr.GetLength(0)-1,j] = vrem;
+    }
+    Console.WriteLine();
+    return arr;
+}
+//либо способ кортежа:
+//(arr[0,1], arr[row-1,i])=(arr[row-1,i], arr[0,i])
+
+
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column, 1, 11);
+Print(arr_1);
+
+ReverseFirstEndRow(arr_1);
+Print(arr_1);
