88d7bed64bedbb8b00a7ee35cdf6fc7323293bf2
Ekaterina <eshirkevich@gmail.com>
2022-08-19 11:35:06 +0300

    hw_1_task

---
+++ 9_lesson/homework/HW_1_task/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет 
+//все чётные натуральные числа в промежутке от M до N.
+//M = 1; N = 5. -> ""2, 4""
+//M = 4; N = 8. -> ""4, 6, 8""
+
+void ValueSet(int min, int max)
+{
+    if (max < min) return;
+    ValueSet(min, max - 1);
+    if (max%2==0)
+    Console.Write($"{max}, ");
+}
+ValueSet(7,13);
