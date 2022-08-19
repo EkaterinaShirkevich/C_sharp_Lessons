656cca164913875f785950e6e098e17bf27470dd
Ekaterina <eshirkevich@gmail.com>
2022-08-19 10:56:21 +0300

    9_2

---
+++ 9_lesson/9_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// // 2. Задайте значения M и N. Напишите программу, которая 
+//    рекурсивно выведет все натуральные числа в промежутке от M до N.
+
+void ValueSet(int min, int max)
+{
+    if (max < min) return;
+    ValueSet(min, max - 1);
+    Console.Write($"{max}, ");
+}
+ValueSet(-9, 8);
+
