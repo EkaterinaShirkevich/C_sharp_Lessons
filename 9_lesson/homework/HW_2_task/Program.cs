88fb6e5dc4da2b29dc1f744b629263168fb1cc85
Ekaterina <eshirkevich@gmail.com>
2022-08-19 12:18:05 +0300

    hw_2_task

--- 9_lesson/homework/HW_2_task/Program.cs
+++ 9_lesson/homework/HW_2_task/Program.cs
@@ -2,3 +2,10 @@
 // найдёт сумму натуральных элементов в промежутке от M до N.
 //M = 1; N = 15 -> 120
 //M = 4; N = 8. -> 30
+
+int SumOfNumbers(int a, int b)
+{
+    if (b < a) return 0;
+    return SumOfNumbers(a, b - 1) + b;
+}
+Console.WriteLine(SumOfNumbers(1, 15));
