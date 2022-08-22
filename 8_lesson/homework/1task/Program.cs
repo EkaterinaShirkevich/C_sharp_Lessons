c7b55ef2713c4614707e7b31445b2978f327c1cf
Ekaterina <eshirkevich@gmail.com>
2022-08-22 15:28:05 +0300

    HW_1_task

--- 8_lesson/homework/1task/Program.cs
+++ 8_lesson/homework/1task/Program.cs
@@ -31,7 +31,20 @@ int[,] MassNums(int row, int column, int from, int to)
     return arr;
 }
 
-
+void OrderToMin(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            for (int k = 0; k < column - j - 1; k++)
+                if (arr[i, k] < arr[i, k + 1])
+                    (arr[i, k], arr[i, k + 1]) = (arr[i, k + 1], arr[i, k]);
+        }
+    }
+}
 
 Console.Write("Enter the number of rows: ");
 int row = int.Parse(Console.ReadLine());
@@ -39,4 +52,6 @@ Console.Write("Enter the number of columns: ");
 int column = int.Parse(Console.ReadLine());
 
 int[,] arr_1 = MassNums(row, column, 1, 11);
+Print(arr_1);
+OrderToMin(arr_1);
 Print(arr_1);
